e6f8e1f2bb7616994d3687d12ac2d2f4b49a2211
NamHyuk Ahn <nmhkahn@gmail.com>
2017-10-12 21:45:54 +0900

    Modify mdrn-v2

--- src/model/mdrn_v2.py
+++ src/model/mdrn_v2.py
@@ -4,8 +4,8 @@ from model import ops
 class MDRN(nn.Module):
     def __init__(self, scale):
         super(MDRN, self).__init__()
-        n_dims = 96
-        n_btn_dims = 64
+        n_dims = 64
+        n_btn_dims = 48
         scale = scale
 
         self.sub_mean = ops.MeanShift((0.4488, 0.4371, 0.4040), sub=True)
@@ -14,13 +14,9 @@ class MDRN(nn.Module):
         self.entry = nn.Conv2d(3, n_dims, 3, 1, 1)
         self.blocks = nn.Sequential(
             *[ops.BtnResBlock(n_dims, n_btn_dims, dilation=1) for _ in range(12)],
-            *[ops.BtnBasicBlock(n_dims, n_btn_dims)],
-            *[ops.BtnMDResBlock(n_dims, [32, 32, 32], [24, 24, 24]) for _ in range(6)],
-            *[ops.BtnBasicBlock(n_dims, n_btn_dims)],
-            *[ops.BtnMDResBlock(n_dims, [32, 32, 32], [24, 24, 24]) for _ in range(6)],
-            *[ops.BtnBasicBlock(n_dims, n_btn_dims)],
+            *[ops.BtnMDResBlock(n_dims, [32, 16, 16], [24, 16, 16]) for _ in range(12)],
             *[ops.BtnResBlock(n_dims, n_btn_dims, dilation=1) for _ in range(12)],
-            *[ops.BtnBasicBlock(n_dims, n_btn_dims)],
+            *[ops.BasicBlock(n_dims, dilation=1) for _ in range(2)],
             nn.Conv2d(n_dims, n_dims, 3, 1, 1),
         )
         self.upsample = ops.UpsampleBlock(n_dims, scale)
