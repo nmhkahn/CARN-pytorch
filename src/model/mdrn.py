98958ffeba384771fae3d9e561bbe96417f391a7
NamHyuk Ahn <nmhkahn@gmail.com>
2017-10-13 18:50:43 +0900

    Modify networks

--- src/model/mdrn.py
+++ src/model/mdrn.py
@@ -14,9 +14,11 @@ class MDRN(nn.Module):
         self.entry = nn.Conv2d(3, n_dims, 3, 1, 1)
         self.blocks = nn.Sequential(
             *[ops.BtnResBlock(n_dims, n_btn_dims, dilation=1) for _ in range(12)],
-            *[ops.BtnResBlock(n_dims, n_btn_dims, dilation=2) for _ in range(12)],
+            *[ops.BasicBlock(n_dims, dilation=1)],
             *[ops.BtnResBlock(n_dims, n_btn_dims, dilation=1) for _ in range(12)],
-            *[ops.BasicBlock(n_dims, dilation=1) for _ in range(2)],
+            *[ops.BasicBlock(n_dims, dilation=1)],
+            *[ops.BtnResBlock(n_dims, n_btn_dims, dilation=1) for _ in range(12)],
+            *[ops.BasicBlock(n_dims, dilation=1)],
             nn.Conv2d(n_dims, n_dims, 3, 1, 1),
         )
         self.upsample = ops.UpsampleBlock(n_dims, scale)
--- src/model/mdrn_v2.py
+++ src/model/mdrn_v2.py
@@ -14,7 +14,9 @@ class MDRN(nn.Module):
         self.entry = nn.Conv2d(3, n_dims, 3, 1, 1)
         self.blocks = nn.Sequential(
             *[ops.BtnResBlock(n_dims, n_btn_dims, dilation=1) for _ in range(12)],
+            *[ops.BasicBlock(n_dims, dilation=1)],
             *[ops.BtnMDResBlock(n_dims, [32, 16, 16], [24, 16, 16]) for _ in range(12)],
+            *[ops.BasicBlock(n_dims, dilation=1)],
             *[ops.BtnResBlock(n_dims, n_btn_dims, dilation=1) for _ in range(12)],
             *[ops.BasicBlock(n_dims, dilation=1) for _ in range(2)],
             nn.Conv2d(n_dims, n_dims, 3, 1, 1),
