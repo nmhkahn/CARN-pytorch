6316e30da1e61974e644e43a2eff0e01beb946a5
NamHyuk Ahn <nmhkahn@gmail.com>
2017-12-01 18:40:57 +0900

    Experiment shell code

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 python src/train.py --patch_size=96 \
                     --batch_size=32 \
-                    --max_steps=300000 \
-                    --decay=200000 \
-                    --model=mdrn \
-                    --ckpt_name=mdrnet-A \
-                    --num_gpu=2 \
-                    --scale=0
+                    --max_steps=200000 \
+                    --decay=100000 \
+                    --model=ex-mdrn-A \
+                    --ckpt_name=ex-mdrn-A \
+                    --scale=2
