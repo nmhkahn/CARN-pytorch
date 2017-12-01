5410be00093acab51d84f332b2b218dac66d9ce2
nmhkahn <nmhkahn@gmail.com>
2017-12-01 16:26:09 +0900

    Fix run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
-python src/train.py --patch_size=96 
-                    --batch_size=32 
-                    --max_steps=300000 
-                    --decay=200000 
-                    --model=mdrn_reduce 
-                    --ckpt_name=mdrnet_reduce
-                    --num_gpu=2 
+python src/train.py --patch_size=96 \
+                    --batch_size=32 \
+                    --max_steps=300000 \
+                    --decay=200000 \
+                    --model=mdrn \
+                    --ckpt_name=mdrnet-A \
+                    --num_gpu=2 \
                     --scale=0
