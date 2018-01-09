2143ce4d49fd86fe1116abe9314cdd1e788d6f03
NamHyuk Ahn <nmhkahn@gmail.com>
2018-01-09 14:35:03 +0900

    Delete temp code

--- run.sh
+++
@@ -1,9 +0,0 @@
-python src/train.py --patch_size=64 \
-                    --batch_size=32 \
-                    --max_steps=300000 \
-                    --decay=200000 \
-                    --model=resnet \
-                    --reduce_upsample=True \
-                    --group=4 \
-                    --ckpt_name=resnet-4-ru \
-                    --scale=2 --num_gpu=2
--- sample.sh
+++
@@ -1,6 +0,0 @@
-model=
-scale=
-
-python src/sample.py --model=$model --ckpt_path=model/$model.pth --sample_dir=sample/ --test_data_dir=dataset/Set14 --scale=$scale
-python src/sample.py --model=$model --ckpt_path=model/$model.pth --sample_dir=sample/ --test_data_dir=dataset/Set5 --scale=$scale
-python src/sample.py --model=$model --ckpt_path=model/$model.pth --sample_dir=sample/ --test_data_dir=dataset/B100 --scale=$scale
